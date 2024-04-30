284d6f6a6841cfb3668f20d6b7ed29ea278df616
winnet101 <115325533+winnet101@users.noreply.github.com>
2024-04-30 09:11:43 -0700

    Redeploy

--- personal-website/src/components/Pokemon.tsx
+++ personal-website/src/components/Pokemon.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { PokeTypes } from "../types";
 import { pokeTypesList } from "../types";
 import styles from "../styles/Pokemon.module.css";
